f37e26ef86593141e23f879be0501cb9f472eadd
Andres Moreno Bernal <andresmorber@gmail.com>
2023-06-19 20:00:05 +0200

    Tablero inicializado con Letras y matriz de Letras

--- MainBoard.gd
+++ MainBoard.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-@onready var mainboard = get_node("/root/MainBoard/MainBoardTileMap")
+@onready var mainboard = get_node("/root/MainBoardTileMap")
 
 #
 func _ready():
@@ -13,24 +13,29 @@ func _ready():
 
 	var map_width = used_rect.size.x
 	var map_height = used_rect.size.y
-	
-	print("Anchura del TileMap: ", map_width)
-	print("Altura del TileMap: ", map_height)
-	
+
 	var EscenaLetra = preload("res://Letter.tscn")
 	var Letra = EscenaLetra.instantiate()
 	
-	var cell = mainboard.get_used_cells(0)[1]
-	
-	var cell_size = mainboard.get_quadrant_size()
-	
-	var cell_index = Vector2(0, 0)  # Índice de la casilla que deseas calcular
+	var first_cell_coords = Vector2(used_rect.position.x, used_rect.position.y)
 
-	var pos = mainboard.map_to_local(cell)
-
+	var tile_size = Vector2(mainboard.tile_set.tile_size) # Obtiene el tamaño de un tile
+	var letters_array = []
+	for x in range(used_rect.position.x, used_rect.position.x + used_rect.size.x):
+		var row = []
+		for y in range(used_rect.position.y, used_rect.position.y + used_rect.size.y):
+			var letter = EscenaLetra.instantiate()
+			
+			var index = Vector2(x,y)
+#			print(index)
+			var cell_coords = mainboard.map_to_local(index)
+			cell_coords.x -=   (tile_size.x) / 2 
+			cell_coords.y -=   (tile_size.y) / 2
+			letter.position = cell_coords
+			mainboard.add_child(letter)  # Añade la instancia a la escena
+			row.append(letter)
+		letters_array.append(row)
+		
 	
 	
-
-	Letra.position = pos
-	mainboard.add_child(Letra)
 
