b44a9bbb18436d3742f4b3a0c1dad4eca7d45cd1
Andres Moreno Bernal <andresmorber@gmail.com>
2024-02-20 14:07:27 +0100

    SceneManager Addon añadido, escenas de reglas del juego tambien

---
+++ src/bresk_rules.gd
@@ -0,0 +1,56 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_back_menu1_button_pressed():
+	SceneManager.no_effect_change_scene("InitialMenu")
+	pass # Replace with function body.
+
+
+func _on_next_button1_pressed():
+	SceneManager.no_effect_change_scene("BreskRules2")
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed():
+	SceneManager.no_effect_change_scene("BreskRules")
+	pass # Replace with function body.
+
+
+func _on_back_button_rules_3_pressed():
+	SceneManager.no_effect_change_scene("BreskRules2")
+	pass # Replace with function body.
+
+
+func _on_next_button2_pressed():
+	SceneManager.no_effect_change_scene("BreskRules3")
+	pass # Replace with function body.
+
+
+func _on_back_button_rules_4_pressed():
+	SceneManager.no_effect_change_scene("BreskRules3")
+	pass # Replace with function body.
+
+
+func _on_next_button_rules_3_pressed():
+	SceneManager.no_effect_change_scene("BreskRules4")
+	pass # Replace with function body.
+
+
+func _on_back_button_rules_5_pressed():
+	SceneManager.no_effect_change_scene("BreskRules4")
+	pass # Replace with function body.
+
+
+func _on_next_button_rules_4_pressed():
+	SceneManager.no_effect_change_scene("BreskRules5")
+	pass # Replace with function body.
